8311d82c240e99aee33498fabb0e8b4710d38331
Muhammad Daniyal <daniyalsuleman61@gmail.com>
2026-01-27 10:17:03 -0500

    update C

--- next.config.mjs
+++ next.config.mjs
@@ -2,28 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 export default nextConfig;
-
--- src/app/build-test/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function TestPage() {
-    return <div>Build Test Success</div>
-}
